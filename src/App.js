08e259cc7ab41d4bdf88a1087585749025f21b69
João Marcus <joaomarcus12769@gmail.com.br>
2020-01-13 23:57:13 -0300

    Adicionando botão 'dismiss'

--- src/App.js
+++ src/App.js
@@ -26,22 +26,37 @@ class App extends React.Component {
     this.state = {
       list: list,
     };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({
+      list: updatedList
+    });
   }
 
   render() {
     return(
-        <div className="App">
-          {this.state.list.map(item =>
-            <div key={item.objectID}>
+      <div className="App">
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
               <span>
-                <a href={item.url}>{item.title}</a>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
+                <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                  Dismiss
+                </button>
               </span>
-            </div>
-          )}
-        </div>
+            </span>
+          </div>
+        )}
+      </div>
     );
   }
 }
