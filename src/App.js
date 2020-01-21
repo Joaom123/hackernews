ea73e9356239e61501900192b831f713a6fbf5a2
João Marcus <joaomarcus12769@gmail.com.br>
2020-01-20 23:43:13 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,64 +1,109 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walk',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walk',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
 ];
 
+const isSearched = (searchTerm) => (item) => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: list,
-    };
-    this.onDismiss = this.onDismiss.bind(this);
-  }
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            list,
+            searchTerm: '',
+        };
 
-  onDismiss(id){
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({
-      list: updatedList
-    });
-  }
+        this.onDismiss = this.onDismiss.bind(this);
+        this.onSearchChange = this.onSearchChange.bind(this);
+    }
 
-  render() {
-    return(
-      <div className="App">
-        {this.state.list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button onClick={() => this.onDismiss(item.objectID)} type="button">
-                  Dismiss
-                </button>
-              </span>
-            </span>
-          </div>
-        )}
-      </div>
-    );
-  }
+    onDismiss(id) {
+        const isNotId = item => item.objectID !== id;
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({
+            list: updatedList
+        });
+    }
+
+    onSearchChange(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+
+    render() {
+        const {list, searchTerm} = this.state;
+
+        return (
+            <div className="App">
+                <Search
+                    value={searchTerm}
+                    onChange={this.onSearchChange}
+                />
+                <Table
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />
+            </div>
+        );
+    }
+}
+
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        );
+    }
+}
+
+class Table extends Component {
+    render() {
+        const {list, pattern, onDismiss} = this.props;
+
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
+                    <div key={item.objectID}>
+                        <a href={item.url}>{item.title}</a>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <button onClick={() => onDismiss(item.objectID)} type="button">
+                                Dismiss
+                            </button>
+                        </span>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
